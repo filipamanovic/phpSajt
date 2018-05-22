49ebaf004e8cf9dffa6b4c9b59be78568bfbfc0c
filip <filipamanovic91@gmail.com>
2018-05-22 15:17:06 +0200

    Pocetak

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Filip Amanović"/>
+    <title class="title">phpSajt</title>
+
+</head>
+<body>
+
+
+</body>
+</html>
